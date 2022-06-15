85ec8af5ad7d02d8719e668194b673bea9cd0b94
Cesar Preza <cpreza93@yahoo.com>
2022-06-14 22:06:45 -0700

    switch statement for button clicked

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let buttonName = document.querySelectorAll('.btn').length;
+let startingTime = 25
 console.log(buttonName);
 
 
@@ -6,10 +7,29 @@ function handleCLick() {
     for (i = 0; i <= buttonName; i++) {
         if (document.querySelectorAll('.btn')[i]) {
             document.querySelectorAll('.btn')[i].addEventListener('click', function () {
-                console.log('clicked')
+                let buttonClicked = this.innerHTML;
+                switch (buttonClicked) {
+                    case 'START':
+                        console.log('start was clicked');
+                        break;
+                    case 'STOP':
+                        console.log('stop was clicked');
+                        break;
+                    case 'RESET':
+                        console.log('reset was clicked');
+                        break;
+                    default:
+                        console.log('idk what was clicked');
+                }
             })
         }
     }
 }
 
+function startTimer() {
+    document.querySelector('.counter').innerHTML = startingTime;
+    
+}
+
 handleCLick();
+startTimer();
